549bc41911a9a2fa27d1d2f04267fa9c7a6e76e2
Paul <paul@originlab.com>
2024-07-25 13:34:01 +0800

    Add doc search controls.

--- src/lib/script.js
+++ src/lib/script.js
@@ -3,7 +3,8 @@ function searchSite(inputId) {
     document.location.href = "https://www.google.com/search?sitesearch=www.originlab.com&q=" + encodeURIComponent(terms);
 }
 
-function searchDoc(inputId, lang, book) {
+function searchDoc(lang, inputId, selectId) {
     var terms = document.getElementById(inputId).value;
+    var book = document.getElementById(selectId).value;
     document.location.href = `https://www.google.com/search?sitesearch=www.originlab.com/doc/${lang}/${book}&` + encodeURIComponent(terms);
 }
