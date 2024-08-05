a0a9db9f8c6f7dc021d44eb9e4a95b3d8b9bde32
Paul <paul@originlab.com>
2024-08-05 11:18:45 +0800

    Move scripts and css into templates.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --channel 8.0
 dotnet run -c Release --project ./src/generator -- ./src/books ./out/static
-cp -r ./src/static/ ./out/static
+cp -r ./src/templates/wwwroot/static/ ./out/static
