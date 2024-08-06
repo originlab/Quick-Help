fa8f9b9a2a0e5c2a402a15997d3ec6f9ecb15ba4
Paul <paul@originlab.com>
2024-08-06 13:23:54 +0800

    --no-verbose

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+wget -nv https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --channel 8.0
 dotnet run -c Release --project ./src/generator -- ./src/books ./out/static
